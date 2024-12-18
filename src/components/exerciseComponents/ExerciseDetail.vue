68379f13967cf466fd583a090b592ac0d48b80e4
Willem Bakker <wimb64@icloud.com>
2024-12-18 16:06:08 +0100

    fix: repair ExerciseDetail by removing JSON parses

--- src/components/exerciseComponents/ExerciseDetail.vue
+++ src/components/exerciseComponents/ExerciseDetail.vue
@@ -11,19 +11,19 @@ defineProps({
   <v-card class="ma-3">
     <v-img
       v-if="exercise.afbeeldingen"
-      :src="JSON.parse(exercise.afbeeldingen).url"
+      :src="exercise.afbeeldingen.url"
       aspect-ratio="16/9"
     />
 
     <v-card-title>{{ exercise.name }}</v-card-title>
 
     <v-card-subtitle>
-      Categories: {{ JSON.parse(exercise.categorie).join(", ") }}
+      Categories: {{ exercise.categorie.join(", ") }}
     </v-card-subtitle>
 
     <v-card-text>
       <p><strong>Description:</strong> {{ exercise.omschrijving }}</p>
-      <p><strong>Age Group:</strong> {{ JSON.parse(exercise.leeftijdsgroep).join(", ") }}</p>
+      <p><strong>Age Group:</strong> {{ exercise.leeftijdsgroep.join(", ") }}</p>
       <p><strong>Duration:</strong> {{ exercise.duur }} minutes</p>
       <p><strong>Min. Players:</strong> {{ exercise.minimum_aantal_spelers }}</p>
 <!--      <p><strong>Requirements:</strong> {{ JSON.parse(exercise.benodigdheden).join(", ") }}</p>-->
