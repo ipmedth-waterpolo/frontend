a9125d759e72e32e8c1004e807fe6f233e18d8e6
Willem Bakker <wimb64@icloud.com>
2025-01-19 04:11:23 +0100

    home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,30 @@
 <script setup lang="ts">
-import { useExercises } from "@/api/composable/useExercises";
-import { onMounted } from "vue";
-const { exercises, error, fetchExercises } = useExercises();
 
-onMounted(() => {
-  fetchExercises();
-});
+const username = localStorage.getItem('username');
+
 </script>
 
 <template>
-  <div v-if="error">
-    {{ error }}
-  </div>
-  <div v-else>
-    <ul>
-      <!-- <li v-for="exercise in exercises" :key="exercise.id">
-        {{ exercise.name }} - {{ exercise.leeftijdsgroep }}
-      </li> -->
-      <!-- <li v-for="exercise in exercises" :key="exercise.id">
-        <span
-          v-for="(category, index) in JSON.parse(exercise.categorie)"
-          :key="index"
-          >{{ category }}
-        </span>
-      </li> -->
-    </ul>
-    {{ exercises }}
-  </div>
+  <v-container>
+    <v-card>
+      <v-card-title>Welkom {{ username }}!</v-card-title>
+      <v-card-text>Dit is de Waterpolo Planner van ZVL-1886. Gebruik deze app om snel een training samen te stellen.
+        Open de zijbalk om te navigeren, of bekijk de lijst hieronder.
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-subtitle>Snelkoppelingen</v-card-subtitle>
+      <v-card-item>
+        <v-list>
+          <v-list-item link :to="{ path: '/mijn-trainingen' }">Mijn trainingen</v-list-item>
+          <v-list-item link :to="{ path: '/oefeningen'}">Training maken (oefeningen overzicht)</v-list-item>
+          <v-list-item link :to="{ path: '/alle-trainingen' }">Alle trainingen</v-list-item>
+          <div class="my-8"></div>
+          <v-list-item link :to="{ path: '/account' }">Account</v-list-item>
+          <v-list-item link :to="{ path: '/about' }">Over de Waterpolo Planner</v-list-item>
+        </v-list>
+      </v-card-item>
+    </v-card>
+  </v-container>
 </template>
 
-<!-- <li v-for="exercise in exercises" :key="exercise.id">
-  {{ exercise.name }} -
-  <span
-    v-for="(category, index) in JSON.parse(exercise.categorie)"
-    :key="index"
-    >{{ category }}</span>
-</li> -->
