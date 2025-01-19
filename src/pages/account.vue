dcaef871561514f5df4dcbb76884781d2c732418
Willem Bakker <wimb64@icloud.com>
2025-01-19 03:34:09 +0100

    account view

--- src/pages/account.vue
+++ src/pages/account.vue
@@ -5,11 +5,32 @@ function logout() {
   window.location.href = '/';
 }
 
+const userData = () => {
+  const username = localStorage.getItem('username');
+  const userID = localStorage.getItem('userID');
+  const userEmail = localStorage.getItem('userEmail');
+  const userRole = localStorage.getItem('userRole');
+  return {username, userID, userEmail, userRole};
+}
+
 </script>
 
 <template>
-
-  <button @click="logout">Uitloggen</button>
+  <v-container>
+    <v-card>
+      <v-card-title>{{ userData().username }}</v-card-title>
+      <v-card-text>
+        <p>E-mail: {{ userData().userEmail }}</p>
+        <p>Jouw rol: {{ userData().userRole }}</p>
+      </v-card-text>
+    </v-card>
+    <v-btn
+      @click="logout"
+      color="error"
+      class="mx-auto my-4"
+    >Uitloggen
+    </v-btn>
+  </v-container>
 
 </template>
 
