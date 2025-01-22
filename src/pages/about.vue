a2943d8f7063faa2ae1ee76e55515f7cfc5d4406
Willem Bakker <wimb64@icloud.com>
2025-01-22 16:20:06 +0100

    feature: add more about text

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -6,7 +6,20 @@
     <v-card>
       <v-card-title>Over de Waterpolo Planner</v-card-title>
       <v-card-text>
-        Deze app is ontwikkeld door studenten van de Hogeschool Leiden
+        Wij zijn een groep studenten van de Hogeschool Leiden die samen hebben gewerkt aan de ontwikkeling van de
+        Waterpolo Planner, een webapplicatie voor waterpolotrainers.
+        <br>
+        <br>
+        Het doel van dit project was om een
+        gebruiksvriendelijke en efficiënte tool te bouwen waarmee trainers snel en eenvoudig trainingen kunnen plannen
+        en oefeningen kunnen selecteren voor hun teams.
+        <br>
+        <br>
+        Het project werd uitgevoerd als onderdeel van de module 'IPMEDTH' voor de opleiding Informatica aan de
+        Hogeschool Leiden en was gericht op het
+        toepassen van onze programmeer- en documentatievaardigheden in een praktijkgerichte opdracht.
+        Het werd uitgevoerd voor Alize Pistidda van ZVL-1886 Waterpolo, waarbij we hun specifieke behoeften voor
+        trainingsplanning als uitgangspunt hebben genomen.
       </v-card-text>
     </v-card>
   </v-container>
